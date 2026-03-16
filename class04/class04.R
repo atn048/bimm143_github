c57cc8d0f1e29b651e75dd85a03c01a15c2035c1
Amy Nguyen <atn048@ucsd.edu>
2026-03-15 17:10:03 -0700

    Add class folders

---
+++ class04/class04.R
@@ -0,0 +1,15 @@
+#' ---
+#' title: "Class 4 R Intro"
+#' author: "Amy Nguyen"
+#' date: "January 15th, 2026"
+#' ---
+
+x <- 1:50
+plot(x)
+plot(x,sin(x))
+
+plot(x,sin(x), typ="l")
+plot(x, sin(x), typ="l", col="pink", lwd=4)
+
+log(10)
+log(10, base = 10)
